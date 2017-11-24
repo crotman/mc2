6a1e643e09bf4105574a6c9d3776bbc4e87426bf
Andre Farzat <andrefarzat@gmail.com>
2017-11-24 13:53:31 -0200

    moarrr things

--- resultados/q5.r
+++ resultados/q5.r
@@ -4,11 +4,10 @@ rm(list=ls())
 setwd("/Users/andrefarzat/Documents/mc2/dados")
 DIGIT <- 4
 
-result <- list()
 data <- read.table("data_t5-t6.txt", header = TRUE)
 
 
-for (i in c(0:5)) {
+for (i in c(0)) {
   instance_name <- paste('I', i, sep="")
   
   subdata <- data[ which(data$config == 'nsga150k2x' & data$inst == instance_name), ]
@@ -21,15 +20,16 @@ for (i in c(0:5)) {
   
   # 
   
-  nsga150k2xse <- data[ which(data$config == 'nsga150k2xse' & data$inst == instance_name), ]
+  subdata <- data[ which(data$config == 'nsga150k2xse' & data$inst == instance_name), ]
   
-  nsga150k2x <- cbind(
+  nsga150k2xse <- cbind(
     best = paste(round(mean(subdata$best), digits=DIGIT), "±", round(sd(subdata$best), digits=DIGIT)),
     hv = paste(round(mean(subdata$hv), digits=DIGIT), "±",round(sd(subdata$hv), digits=DIGIT)),
     gd = paste(round(mean(subdata$gd), digits=DIGIT), "±", round(sd(subdata$gd), digits=DIGIT))
   )
   
-  result[instance_name] <- cbind(nsga150k2x, nsga150k2xse)
+  # result <- matrix(data=c(nsga150k2x, nsga150k2xse), nrow=3, ncol=2)
+  
+  print(nsga150k2x)
+  print(nsga150k2xse)
 }
-
-summary(result)
