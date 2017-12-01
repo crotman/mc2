be059e3d67729ee647d8ec5e582f5549cf2348ab
Andre Farzat <andrefarzat@gmail.com>
2017-12-01 18:54:37 -0200

    wip

--- resultados/q4.r
+++ resultados/q4.r
@@ -1,8 +1,6 @@
 # Gerar a análise de tamanho de efeito:
 # comparativo de qualidade para cada instância e pares de abordagens NSGAII x MAR , NSGAII x SH e NSGAII x CPM (página 1065).
 
-# based on: https://www.leeds.ac.uk/educol/documents/00002182.htm
-
 rm(list=ls())
 setwd("/Users/andrefarzat/Documents/mc2/dados")
 DIGIT <- 4
@@ -17,16 +15,43 @@ for (i in c('CPM', 'MAR', 'SH', 'nsga150k2x')) {
     hv = c(round(mean(subdata$hv), digits=DIGIT), round(sd(subdata$hv), digits=DIGIT)),
     gd = c(round(mean(subdata$gd), digits=DIGIT), round(sd(subdata$gd), digits=DIGIT))
   )
+  
+  print(result);
+}
+
+# http://doofussoftware.blogspot.com.br/2012/07/measuring-effect-size-with-vargha.html
+AMeasure <- function(a, b){
+  
+  # Compute the rank sum (Eqn 13)
+  r = rank(c(a,b))
+  r1 = sum(r[seq_along(a)])
+  
+  # Compute the measure (Eqn 14) 
+  m = length(a)
+  n = length(b)
+  A = (r1/m - (m+1)/2)/n
+  
+  A
 }
 
 effectsize <- function (one, two) {
   effectsize_result <- cbind(
-    best = (one[1,][1] - two[1,][1]) / one[2,][1],
-    hv = (one[1,][2] - two[1,][2]) / one[2,][2],
-    gd = (one[1,][3] - two[1,][3]) / one[2,][3]
+    best = each_effectsize(one[1,][1], two[2,][1]),
+    hv = each_effectsize(one[1,][2], two[2,][2]),
+    gd = each_effectsize(one[1,][3], two[2,][3])
+    
+    #best = (one[1,][1] - two[1,][1]) / one[2,][1],
+    #hv = (one[1,][2] - two[1,][2]) / one[2,][2],
+    #gd = (one[1,][3] - two[1,][3]) / one[2,][3]
   )
 
   return(effectsize_result)
+}
+
+each_effectsize <- function(r1, r2) {
+  m <- length(r1);
+  n <- length(r2);
+  return ((sum(rank(c(r1, r2)))[seq_along(r1)] / m - (m + 1) / 2) / n);
 }
 
 print(effectsize(result$nsga150k2x, result$CPM))
